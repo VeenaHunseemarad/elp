42696080cdbc1bde02557e8026b6d31e85da4934
veena <veenahunseemarad32@gmail.com>
2023-11-24 01:04:31 +0530

    dm

---
+++ demo/src/demo/d.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class d {
+
+	public static void main(String[] args) {
+System.out.println("Hi im veena");
+	}
+
+}
---
+++ demo/src/demo/d2.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class d2 {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+System.out.println(a+b);
+	}
+
+}
